81e8b6c9283894e3310cca3f1d5ab2cbd51d04e9
TymonJones <101831045+Tjonte03@users.noreply.github.com>
2023-04-07 15:02:43 -0700

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+function add(a, b){
+    return a + b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+
+function subtract(a, b){
+    return a - b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function increment(n){
+return n +=1;
+}
+
+function decrement(n){
+return n -=1;
+}
+
+function makeInt(str) {
+return parseInt(str, 10);
+}
+
+function preserveDecimal(str){
+return parseFloat(str);
+}
